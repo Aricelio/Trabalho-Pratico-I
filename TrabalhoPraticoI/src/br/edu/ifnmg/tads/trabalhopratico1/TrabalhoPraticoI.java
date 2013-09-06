955401f569560a6c5b1e38df4fcf1b49800521e2
alisson 02 <alisson 02@192.168.0.2>
2013-09-06 03:41:48 -0300

    Classe Pronta

--- TrabalhoPraticoI/src/br/edu/ifnmg/tads/trabalhopratico1/TrabalhoPraticoI.java
+++ TrabalhoPraticoI/src/br/edu/ifnmg/tads/trabalhopratico1/TrabalhoPraticoI.java
@@ -16,7 +16,15 @@ public class TrabalhoPraticoI {
     public static void main(String[] args) 
     {
         // TODO code application logic here
-        private int id;
-        private int idade;
+        Funcionario ari = null;
+        
+        ari.setNome("Aricelio");
+        ari.setCargo("Instrutor");
+        ari.setCodigo(2);
+        ari.setCpf("115.825.116-59");
+        ari.setRg("17.791.878");
+        ari.setEndereco("Rua Coronel");
+        
+        System.out.print(ari.toString());        
     }
 }
